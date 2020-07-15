1de4f24164602c922256ac2d637f4d7f0233d3ce
Darrin Speegle <speegled@slu.edu>
2020-07-15 16:31:08 -0500

    `cows` data set and documentation

---
+++ R/cows.R
@@ -0,0 +1,14 @@
+#' Cooling down hot cows.
+#'
+#' Cows in California were sprayed down with water using various nozzles. The temperature of the cows
+#' was measured at various body parts and time intervals. Descriptions due to the authors of the study.
+#'
+#' @format
+#' \describe{
+#' \item{cow}{ID of the cow that was measured.}
+#' \item{date}{date that the cow was treated.}
+#' \item{nozzle}{Model number of the 6 nozzles tested; control indicates treatment with no spray; each cow received each treatment 3 times}
+#' \item{flow_rate}{Rate at which water was sprayed on cows, in liters per minute.}
+#' \item{body_part}{The body part of the cow where the temperature (degrees Celsius) was taken.}
+#' \item{min_01, min_02, min_03, min_04}{Value of the skin temperature measurement recorded 1, 2, 3 or 4 minutes after the first 3-min spray application began.}
+#' @source \url{https://zenodo.org/record/3862181#.XwTRNpNKjEa}
