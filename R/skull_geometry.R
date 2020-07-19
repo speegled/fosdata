a37f3d691754f504d39f935115ffdc7f41fc724f
Darrin Speegle <speegled@slu.edu>
2020-07-19 18:41:36 -0500

    adding skull geometry

---
+++ R/skull_geometry.R
@@ -0,0 +1,16 @@
+#' Skull Geometry
+#'
+#' Measurements of circumference of skulls of children at various ages.
+#'
+#' From the authors: "Head injury is the leading cause of fatality and long-term disability for children. Pediatric heads change rapidly in both size and shape during growth, especially for children under 3 years old."
+#'
+#' @format A data frame with 56 observations of 2 variables. Descriptions of variables are from the authors.
+#' \describe{
+#'   \item{age_mos}{age of child in months}
+#'   \item{circumference_cm}{circumference of skull in centimeters}
+#' }
+#'
+#' @source Li, Zhigang et al. (2016), Data from: A statistical skull geometry model for children 0-3 years old, Dryad, Dataset, https://doi.org/10.5061/dryad.p4p62
+#' @source \url{https://datadryad.org/stash/dataset/doi:10.5061/dryad.p4p62}
+#' @source \url{https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0127322}
+"skull_geometry"
