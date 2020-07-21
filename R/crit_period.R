03c6f7cdf00ab87b4cbb9e80b8ff2cdd4648f905
Darrin Speegle <speegled@slu.edu>
2020-07-21 12:32:17 -0500

    cph

---
+++ R/crit_period.R
@@ -0,0 +1,16 @@
+#' Critical Period Hypothesis
+#'
+#' The age of onset of acquisition and ultimate level of attainment of second language learners in North America and Israel.
+#'
+#' From the author: "In second language acquisition research, the critical period hypothesis (cph) holds that the function between learners' age and their susceptibility to second language input is non-linear. This paper revisits the indistinctness found in the literature with regard to this hypothesis's scope and predictions."
+#'
+#' @format A data frame with 30 observations of 2 variables. Descriptions of variables are from the authors.
+#' \describe{
+#'   \item{aoa}{age of onset of acquisition of second language. In years.}
+#'   \item{gjt}{grammaticality judgement test. A measure of second language proficiency.}
+#'   \item{locale}{Factor with two levels. "North America" and "Israel"}
+#' }
+#'
+#' @source Vanhove J (2013) The Critical Period Hypothesis in Second Language Acquisition: A Statistical Critique and a Reanalysis. PLoS ONE 8(7): e69172.
+#' @source \url{https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0069172}
+"crit_period"
