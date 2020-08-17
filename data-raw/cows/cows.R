2fde3729861c9eeae50045a7c8555b19c9dce965
Darrin Speegle <speegled@slu.edu>
2020-08-17 12:36:06 -0500

    cows_small was grouped

--- data-raw/cows/cows.R
+++ data-raw/cows/cows.R
@@ -2,8 +2,9 @@ cows <- read.csv("data-raw/cows/cows.csv")
 cows_small <- filter(cows, nozzle %in% c("TK-0.75", "Control", "TK-12"), body_part == "shoulder") %>%
   group_by(cow, nozzle) %>%
   summarize(temp_change = mean(min_04 - min_01),
-            cow = first(cow))
-cows_small <- cows_small %>% pivot_wider(names_from = "nozzle", values_from = temp_change)
+            cow = first(cow)) %>%
+  ungroup()
+cows_small <- cows_small %>% tidyr::pivot_wider(names_from = "nozzle", values_from = temp_change)
 cows_small <- janitor::clean_names(cows_small)
 save(list = c("cows_small"), file = "data/cows_small.rda")
 plot(cows_small$tk_0_75, cows_small$tk_12)
