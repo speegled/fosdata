c1760b0a4a04022e80e5567031e511cb2e3be5c6
Darrin Speegle <speegled@slu.edu>
2020-12-15 15:58:14 -0600

    the future is plastics

---
+++ data-raw/plastics/plastics.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+
+ee <- readxl::read_xlsx("data-raw/plastics/aax1157_Table_S3.xlsx", sheet = 2)
+ee <- ee %>%
+  janitor::clean_names()
+ee <- ee %>%
+  mutate(colour = tolower(colour)) %>%
+  mutate(colour = str_replace(colour, "dark[- /]blue", "darkblue")) %>%
+  mutate(colour = str_replace(colour, "ligth", "light")) %>%
+  mutate(colour = str_replace_all(colour, " ", "")) %>%
+  mutate(colour = str_replace_all(colour, "-|–", "/")) %>%
+  mutate(colour = str_replace(colour, "wine/red", "winered")) %>%
+  mutate(colour = str_replace(colour, "lila", "lilac")) %>%
+  mutate(colour = str_replace(colour, "turquios|turquois", "turquoise")) %>%
+  select(-fiber_id)
+
+ee <- as.data.frame(ee)
+plastics <- ee
+save(plastics, "data/plastics.rda")
