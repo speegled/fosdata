8c338ac20f89c603c4d4f28bc0b909409581c4f5
Bryan Clair <bryan@slu.edu>
2020-12-30 14:35:21 -0600

    Adding scrabble tiles dataset

---
+++ R/scrabble.R
@@ -0,0 +1,12 @@
+#' Scrabble tile values
+#'
+#' This dataset contains 100 records corresponding to the 100 tiles in English language editions of the board game Scrabble.
+#'  
+#' @format A data frame with 100 observations of 2 variables
+#' \describe{
+#'   \item{piece}{the letter A-Z shown on the tile or "blank"}
+#'   \item{points}{the points shown on the tile}
+#' }
+#' 
+#' @source \url{https://en.wikipedia.org/wiki/Scrabble_letter_distributions}
+"scrabble"
