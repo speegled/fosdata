25d0c402715d9f3159c994031ba8061a2bee1b62
Bryan Clair <bryan@slu.edu>
2021-03-23 03:21:21 -0500

    Add variables to match the help page.  Add help to match the variables. Reorder variables to match help.  Convert to dataframe (instead of tibble).

--- R/sharks.R
+++ R/sharks.R
@@ -5,7 +5,7 @@
 #' From the authors: "Despite the ongoing need for shark conservation and management, prevailing negative sentiments marginalize these animals and legitimize permissive exploitation. These negative attitudes arise from an instinctive, yet exaggerated fear, which is validated and reinforced by disproportionate and sensationalistic news coverage of shark ‘attacks’ and by highlighting shark-on-human violence in popular movies and documentaries. In this study, we investigate another subtler, yet powerful factor that contributes to this fear: the ominous background music that often accompanies shark footage in documentaries."
 #'
 #'
-#' @format A data frame with 732 observations of 19 variables. For variables with values from 1-7 without description, 1 = not at all and 7 = very much
+#' @format A data frame with 616 observations of 15 variables. For variables with values from 1-7 without description, 1 = not at all and 7 = very much
 #' \describe{
 #'   \item{av}{did the subject see video or listen to audio}
 #'   \item{music}{was the music played to subject uplifting, ominous or silence}
@@ -15,10 +15,11 @@
 #'   \item{peaceful}{how well does peaceful describe sharks 1-7}
 #'   \item{beautiful}{how well does beautiful describe sharks 1-7}
 #'   \item{graceful}{how well does graceful describe sharks 1-7}
+#'   \item{free_response}{an adjective (or more) that describes sharks}
 #'   \item{conserve}{willingness to conserve from 1-7}
 #'   \item{gender}{1 male, 2 female}
 #'   \item{age}{age in years}
-#'   \item{race_ethnicity}{1 white_or_caucasian 2 black_or_african_american 3 hispanic_or_latino 4 asian 5 american_indian_or_alaskan_native 6 native_hawaiian_or_pacific_islander 7 other}
+#'   \item{race_ethnicity}{1 white_or_caucasian, 2 black_or_african_american, 3 hispanic_or_latino, 4 asian, 5 american_indian_or_alaskan_native, 6 native_hawaiian_or_pacific_islander, 7 other}
 #'   \item{annual_income}{1-9 by increments of 25K, so 1 = 0-25K, 2 = 25-50K, ..., 9 = 200K+}
 #'   \item{political_views}{levels 1-7 with 1 extremely liberal, 4 moderate and 7 extremely conservative}
 #' }
--- data-raw/sharks/sharks.R
+++ data-raw/sharks/sharks.R
@@ -1,17 +1,21 @@
-dd <- readxl::read_xlsx("data-raw/sharks/S1Dataset.XLSX")
+# Set directory to match this script
+setwd(dirname(parent.frame(2)$ofile))
+
+dd <- readxl::read_xlsx("S1Dataset.XLSX")
 dd <- janitor::clean_names(dd)
 names(dd)
 library(dplyr)
-dd <- select(dd, matches("^experimen|^scar|^dang|^vici|^peace|^bea|^grace|^free|gender|^age|^race|^pol"))
-names(dd) <- c("treatment", "scary", "dangerous", "vicious", "peaceful", "beautiful", "graceful", "free_response", "gender", "age", "race", "political_views")
+dd <- select(dd, matches("^experimen|^scar|^dang|^vici|^peace|^bea|^grace|^free|^willing|gender|^age|^race|^annual|^pol"))
+names(dd) <- c("treatment", "scary", "dangerous", "vicious", "peaceful", "beautiful", "graceful", "free_response", "conserve", "gender", "age", "race_ethnicity", "annual_income", "political_views")
 dd$av <- ifelse(dd$treatment <= 3, "video", "audio")
 dd$music <- ifelse(dd$treatment %in% c(1, 4), "uplifting", ifelse(dd$treatment %in% c(2, 5), "ominous", "silence"))
 dd$treatment <- NULL
 dd$av <- factor(dd$av)
 dd$music <- factor(dd$music)
+dd <- relocate(dd, av, music)
 summary(dd)
-sharks <- dd
-save(list = c("sharks"), file = "data/sharks.rda")
+sharks <- data.frame(dd)
+save(list = c("sharks"), file = "sharks.rda")
 
 filter(sharks, av == "video", music %in% c("ominous", "uplifting")) %>%
   with(wilcox.test(vicious ~ music))
