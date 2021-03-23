54ca764699e71238d29ee3b02ff1a2f55c81041c
Bryan Clair <bryan@slu.edu>
2021-03-23 02:39:12 -0500

    This data was sacreligiously unclean, to the point where it broke reasonable code. There's now a script that takes the raw data from MIT and converts it to a clean dataframe. Also dropped two useless variables and updated the man page.

--- R/pres_election.R
+++ R/pres_election.R
@@ -2,19 +2,17 @@
 #'
 #' County level results for presidential elections in the USA from 2000-2016.
 #'
-#' @format A data frame with 50524 observations of 11 variables.
+#' @format A data frame with 50524 observations of 9 variables.
 #' \describe{
-#'   \item{year}{Year of election}
-#'   \item{state}{State of results}
-#'   \item{state_po}{State Postal Abbreviation}
-#'   \item{county}{County within state}
-#'   \item{FIPS}{FIPS code for the county. Useful due to non-standard abbreviations of counties}
-#'   \item{office}{Office for which the election was held}
-#'   \item{candidate}{Name of candidate}
-#'   \item{party}{Political party of the candidate}
-#'   \item{candidatevotes}{Votes that candidate received}
-#'   \item{totalvotes}{Total votes cast in county}
-#'   \item{version}{Value 20191203}
+#'   \item{year}{Year of election.}
+#'   \item{state}{State of results.}
+#'   \item{state_po}{State postal abbreviation.}
+#'   \item{county}{County within state.}
+#'   \item{FIPS}{FIPS code for the county. Useful due to non-standard abbreviations of counties.}
+#'   \item{candidate}{Name of candidate.}
+#'   \item{party}{Political party of the candidate.}
+#'   \item{candidatevotes}{Votes that candidate received in the county.}
+#'   \item{totalvotes}{Total votes cast in the county.}
 #' }
 #'
 #' @source MIT Election Data and Science Lab, 2018, "County Presidential Election Returns 2000-2016", https://doi.org/10.7910/DVN/VOQCHQ, Harvard Dataverse, V6, UNF:6:ZZe1xuZ5H2l4NUiSRcRf8Q== [fileUNF]
