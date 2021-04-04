0c3cb8d34221030f51b59e3b9fa0fc4c33850212
Darrin Speegle <speegled@slu.edu>
2021-04-04 09:56:57 -0500

    world cup data and sauce

---
+++ R/world_cup.R
@@ -0,0 +1,18 @@
+#' FIFA World Cup 2014-2015 Results
+#'
+#' Results from the FIFA 2014 Men's World Cup and the FIFA 2015 Women's World Cup finals.
+#'
+#' @format A data frame with 228 observations of 5 variables.
+#' \describe{
+#'   \item{team_1}{Winning team}
+#'   \item{score_1}{Number of goals for winning team}
+#'   \item{team_2}{Losing team}
+#'   \item{score_2}{Number of goals for losing team}
+#'   \item{game}{Description of when the game took place. Group Stage and Group [A-F] are equivalent level games}
+#'   \item{date}{Date of game}
+#'   \item{notes}{Notes on game, such as overtime or penalties}
+#'   \item{competition}{One of 2014 FIFA Men's World Cup or 2015 FIFA Women's World Cup}
+#' }
+#'
+#' @source https://abcnews.go.com/Sports/2014-fifa-world-cup-tv-schedule-results/story?id=23748377 and https://abcnews.go.com/Sports/2015-fifa-womens-world-cup-complete-tournament-results/story?id=31204768
+"world_cup"
