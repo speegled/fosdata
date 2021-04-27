068f10e356315085175def05e8dafc3c0ab8ec12
Bryan Clair <bryan@slu.edu>
2021-04-27 01:48:47 -0500

    Adding draft 1970 dataset.

---
+++ R/draft.R
@@ -0,0 +1,15 @@
+#' The 1970 Draft Lottery
+#'
+#' Results of the draft lottery held December 1, 1969 to determine the order in which young men would be drafted in 1970 for service in the Vietnam war.
+#'
+#' @format A data frame with 366 observations of 5 variables.
+#' \describe{
+#'   \item{Day}{Day of the month.}
+#'   \item{Month}{Month abbreviation.}
+#'   \item{MonthNo}{Month number 1-12.}
+#'   \item{DayofYear}{Day of year from 1-366.}
+#'   \item{DraftNo}{Draft number from 1-366. Lowest numbers were drafted first.}
+#' }
+#'
+#' @source United States Government Selective Service System.  https://www.sss.gov/history-and-records/vietnam-lotteries/
+"draft"
