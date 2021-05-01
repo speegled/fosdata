4e4fd2c45f49e2540fb697cf7e012825aecc005e
Bryan Clair <bryan@slu.edu>
2021-05-01 16:53:43 -0500

    Cannot use latex in these :-(

--- R/acorns.R
+++ R/acorns.R
@@ -6,11 +6,11 @@
 #' \describe{
 #'   \item{Species}{Oak species.}
 #'   \item{Region}{Area of prevalence. Atlantic or California.}
-#'   \item{Range}{Geographic range area, km$^2 \times 100$.}
-#'   \item{Acorn_size}{In cm$^3$.  Estimated from Sargent (1961) except trees 4, 15, 16, 33.}
+#'   \item{Range}{Geographic range area, km^2 x 100.}
+#'   \item{Acorn_size}{In cm^3.  Estimated from Sargent (1961) except trees 4, 15, 16, 33.}
 #'   \item{Tree_height}{In meters. Estimated from Sargent (1961) except trees 16, 38.}
 #' }
 #'
-#' @details From the authors: "Seed size can be an important factor influencing the extent of the range of a plant species that has recently expanded or shifted the area it occupies as a consequence of climatic changes. A positive correlation exists between acorn size and geographical range of the North American oaks (Quercus L.) of eastern North America. An independent analysis of oaks native to the California region suggests that this is a continental pattern."  Oak 39, Quercus tomentella Engelm, is only found on islands off the western coast.
+#' @details From the authors: "Seed size can be an important factor influencing the extent of the range of a plant species that has recently expanded or shifted the area it occupies as a consequence of climatic changes. A positive correlation exists between acorn size and geographical range of the North American oaks (Quercus L.) of eastern North America. An independent analysis of oaks native to the California region suggests that this is a continental pattern."  Oak 39, \emph{Quercus tomentella Engelm}, is only found on islands off the western coast.
 #' @source Aizen, Marcelo & Patterson III, William. (1990). Acorn Size and Geographical Range in the North American Oaks (Quercus L.). Journal of Biogeography. 17. 327. 10.2307/2845128. https://www.jstor.org/stable/2845128 
 "acorns"
