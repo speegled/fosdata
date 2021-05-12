9128b6384686b5ae32e6d16902de2ff92c8831ce
Bryan Clair <bryan@slu.edu>
2021-05-12 01:38:58 -0500

    Reproduce analysis from paper with mcnemar.test

--- data-raw/mask_influenza/masks.R
+++ data-raw/mask_influenza/masks.R
@@ -1,16 +1,44 @@
+library(dplyr)
+
+# load and clean data
 dd <- read.csv("data-raw/mask_influenza/Table_S1.txt", sep = " ", header = FALSE)
 dd <- as.data.frame(matrix(dd$V1, ncol = 7, byrow = T))
 names(dd) <- dd[1,]
 dd <- dd[-1,]
-library(dplyr)
 dd <- dd %>% mutate(across(-matches("PCR"), as.numeric))
 dd$PCR_type <- factor(dd$PCR_type)
-summary(dd)
 
-wilcox.test(dd$Mask_Fine, dd$No_Mask_Fine, paired = TRUE)
-t.test(log(dd$Mask_Coarse + 1), log(dd$No_Mask_Coarse + 1), paired = TRUE)
-
-boxplot(log(dd$No_Mask_Fine+1))
 masks <- dd
 masks <- janitor::clean_names(masks)
-save(masks, file = "data/masks.rda")
+
+# save(masks, file = "data/masks.rda")
+
+# examples
+summary(masks)
+boxplot(log(masks$no_mask_fine+1), log(masks$mask_fine+1))
+
+wilcox.test(masks$mask_fine, masks$no_mask_fine, paired = TRUE)
+t.test(log(masks$mask_fine + 1), log(masks$no_mask_fine + 1), paired = TRUE)
+
+# reproduce analysis from the paper
+fine_tab <- with(masks, table(mask_fine > 0, no_mask_fine > 0))
+coarse_tab <- with(masks, table(mask_coarse > 0, no_mask_coarse > 0))
+both_tab <- with(masks, table(mask_fine + mask_coarse > 0, no_mask_fine + no_mask_coarse > 0))
+
+fine_tab
+coarse_tab
+both_tab
+
+mcnemar.test(fine_tab, correct=FALSE)
+mcnemar.test(coarse_tab, correct=FALSE)
+mcnemar.test(both_tab, correct=FALSE)
+
+# these are the same as prop.test with the off-diagonal table entries
+prop.test(6,  7,  p=0.5, correct=FALSE) # p = 0.05878
+prop.test(14, 16, p=0.5, correct=FALSE) # p = 0.0027
+prop.test(6,  6,  p=0.5, correct=FALSE) # p = 0.01431
+
+# Using binom.test is quite different
+binom.test(6,  7,  p=0.5)  # p = 0.125
+binom.test(14, 16, p=0.5)  # p = 0.004181
+binom.test(6 , 6,  p=0.5)  # p = 0.03125
