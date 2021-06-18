574d4917f235dd451bc2050b42749ac9066e30dc
Bryan Clair <bryan@slu.edu>
2021-06-18 16:02:27 -0500

    Wrong dims for cows_small.

--- R/cows.R
+++ R/cows.R
@@ -17,6 +17,7 @@
 #'   \item{min_01, min_02, min_03, min_04}{Value of the skin temperature measurement recorded 1, 2, 3 or 4 minutes after the first 3-min spray application began.}
 #' }
 #'
+#' @source Tucker, Cassandra B., Schütz, Karin E., & Van Os (Chen), Jennifer M. (2020). Data from: Cooling cows efficiently with sprinklers: physiological responses to water spray [Data set].
 #' @source \url{https://zenodo.org/record/3862181#.XwTRNpNKjEa}
 "cows"
 
@@ -28,7 +29,7 @@
 #' From the authors: "Dairies in the United States commonly cool cattle with sprinklers mounted over the feed bunk that intermittently spray the cows' backs. These systems use potable water—an increasingly scarce resource—but there is little experimental evidence about how much is needed to cool cows or about droplet size, which is thought to affect hair coat penetration. "
 #'
 #'
-#' @format A data frame with 798 observations of 9 variables
+#' @format A data frame with 19 observations of 4 variables
 #' \describe{
 #'   \item{cow}{ID of the cow that was measured.}
 #'   \item{control}{Shoulder temperature change (degrees Celsius) after 3 minutes when no water was sprayed.}
